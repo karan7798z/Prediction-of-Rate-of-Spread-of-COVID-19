1cba5e76de9bc0b0ff0ae8ce763ae75f12cdf791
karan7798z <karan7798z@gmail.com>
2020-11-29 17:48:42 -0600

    Modified Model

--- Best_Model.R
+++ Best_Model.R
@@ -33,14 +33,7 @@ xgbGrid <- expand.grid(nrounds = c(400,600,800), #no of iterations
 )
 
 
-### 3.2 Converting full provided test data to a DMatrix format, as required by the XGBoost Library
-
-set.seed(122)
-X_test_full = xgb.DMatrix(as.matrix(test_data %>% select(-`Mean(R)`)))
-y_test_full = test_data$`Mean(R)`
-
-
-### 3.3 Fitting/Training multiple XGBoost Models on complete train data for each set of hyperparameter value from the hyperparameter grid. Test MSE will be computed for each model, and at the end model with least Test MSE will be chosen. 
+### 3.2 Fitting/Training multiple XGBoost Models on complete train data for each set of hyperparameter value from the hyperparameter grid. Test MSE will be computed for each model, and at the end model with least Test MSE will be chosen. 
 
 xgbGrid$Test_MSE <- 1 #Adding a new column in xgbGrid for storing Test MSE for each row (i.e. each set of hyperparameter values)
 
@@ -50,7 +43,7 @@ for (i in 1:nrow(xgbGrid)){
   set.seed(122)
   xgb_model_full_grid <- xgboost(
     data.matrix(data[,-1]), 
-    label = y_train_full,
+    label = data$Mean.R.,
     nround = xgbGrid$nrounds[i],
     max_depth = xgbGrid$max_depth[i],
     colsample_bytree = xgbGrid$colsample_bytree[i],
@@ -58,19 +51,19 @@ for (i in 1:nrow(xgbGrid)){
     subsample = xgbGrid$subsample[i]
   )
   
-  pred_values <- predict(xgb_model_full_grid, X_test_full)
-  Test_MSE <- mean((y_test_full - pred_values)^2)
+  pred_values <- predict(xgb_model_full_grid, data.matrix(test_data[,-1]))
+  Test_MSE <- mean((pred_values - test_data$`Mean(R)`)^2)
   xgbGrid$Test_MSE[i] <- Test_MSE
   print(i)
 }
 
 
-### 3.4 Fitting/Training a XGBoost Model on complete train data for hyperparameters are as chosen by the above grid for lowest Test MSE
+### 3.3 Fitting/Training a XGBoost Model on complete train data for hyperparameters are as chosen by the above grid for lowest Test MSE
 
 set.seed(122)
 xgb_model_best_grid <- xgboost(
   data.matrix(data[,-1]), 
-  label = y_train_full,
+  label = data$Mean.R.,
   nround = xgbGrid$nrounds[which.min(xgbGrid$Test_MSE)],
   max_depth = xgbGrid$max_depth[which.min(xgbGrid$Test_MSE)],
   colsample_bytree = xgbGrid$colsample_bytree[which.min(xgbGrid$Test_MSE)],
@@ -79,9 +72,9 @@ xgb_model_best_grid <- xgboost(
 )
 
 
-### 3.5 Final Test MSE using the best model as fitted above.
+### 3.4 Final Test MSE using the best model as fitted above.
 
 set.seed(122)
-predicted_test_full = predict(xgb_model_best_grid, X_test_full)
-test_full_mse_xgb = mean((predicted_test_full - y_test_full)^2)
-print(paste("The Final Test MSE for Xtreme Gradient Boosting is:",test_full_mse_xgb))
+predicted_test_data = predict(xgb_model_best_grid, data.matrix(test_data[,-1]))
+test_mse_xgb = mean((predicted_test_data - test_data$`Mean(R)`)^2)
+print(paste("The Final Test MSE for Xtreme Gradient Boosting is:",test_mse_xgb))
